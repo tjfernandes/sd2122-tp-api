2d002a17f571e3b320397304736840049bfd4b97
smduarte <smd@fct.unl.pt>
2022-04-08 16:14:21 +0100

    Update Files.java

--- src/tp1/api/service/util/Files.java
+++ src/tp1/api/service/util/Files.java
@@ -10,11 +10,11 @@ public interface Files {
 	 * @param token - token for accessing the file server (in the first 
 	 * project this will not be used).
      *
-	 * @return 204 if success.
-	 *         403 if the password is incorrect.
-	 * 		   400 otherwise.
+	 * @return OK if success.
+	 *         FORBIDDEN if the token is incorrect.
+	 * 	   BAD_REQUEST otherwise.
 	 */
-	void writeFile(String fileId, byte[] data, String token);
+	Result<Void> writeFile(String fileId, byte[] data, String token);
 
 	/**
 	 * Delete an existing file.
@@ -23,12 +23,12 @@ public interface Files {
 	 * @param token - token for accessing the file server (in the first 
 	 * project this will not be used).
 	 * 
-	 * @return 204 if success; 
-	 *		   404 if the uniqueId does not exist.
-	 *         403 if the password is incorrect.
-	 * 		   400 otherwise.
+	 * @return OK if success; 
+	 *	   NOT_FOUND if the fileId does not exist.
+	 *         FORBIDDEN if the token is incorrect.
+	 * 	   BAD_REQUEST otherwise.
 	 */
-	void deleteFile(String fileId, String token);
+	Result<Void> deleteFile(String fileId, String token);
 
 	/**
 	 * Get the contents of the file. 
@@ -37,10 +37,10 @@ public interface Files {
 	 * @param token - token for accessing the file server (in the first 
 	 * project this will not be used).
 	 * 
-	 * @return 200 if success + contents (through redirect to the File server); 
-	 *		   404 if the uniqueId does not exist.
-	 *         403 if the password is incorrect.
-	 * 		   400 otherwise.
+	 * @return OK if success + contents (through redirect to the File server); 
+	 *	   NOT_FOUND if the uniqueId does not exist.
+	 *         FORBIDDEN if the password is incorrect.
+	 * 	   BAD_REQUEST otherwise.
 	 */
 	Result<byte[]> getFile(String fileId, String token);
 
